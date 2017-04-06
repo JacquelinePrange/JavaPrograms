51802baa1ab3b5a4a65fcc40f6f94dee03c01c24
Jacqueline <Jacqueline.prange@assurity.co.nz>
2017-04-07 11:29:27 +1200

    initial commit

---
+++ FloatAndDouble/src/com/Jaxs/Main.java
@@ -0,0 +1,27 @@
+package com.Jaxs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myIntValue = 5 / 3;
+	    //with int = 32 (4 bytes).
+
+	    float myFloatValue = 5f / 3f;
+        // width of float = 32 (4 bytes).
+
+	    double myDoubleValue = 5d / 3d;
+	    //width of double = 64 (8 bytes).
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+        //Convert a given number of pounds to kilograms
+        //1. Create a variable store the number of pounds
+        // 2. Calculate the number of Kilograms for the number above and store in a variable.
+        // 3. Print the result.
+        //
+        // Notes: 1 pound is equal to 0.45359237 kilograms.
+
+    }
+}
---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Jacqueline on 5/04/2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Jaxs!");
+        int myFirstNumber = 200+900+100;
+        int mySecoundNumber = 12;
+        int myThirdNumber = myFirstNumber + 6;
+
+        int myTotal = myFirstNumber + mySecoundNumber + myThirdNumber;
+
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myLastOne);
+    }
+}
